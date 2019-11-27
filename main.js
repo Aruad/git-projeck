eb0ebc88a7f9b7c0decbee59334713a81c866845
Aruad <57110920+Aruad@users.noreply.github.com>
2019-11-27 13:17:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-alert('hello')
+function toStr(el){
+    var ret='';
+    el.forEach(function(e) {
+        ret+=e;
+        //console.log(e);
+    });
+    return ret;
+}
+//
+var t=document.getElementById('target');
+var a = new Array('*');
+//var b = new Array();
+t.innerHTML+=a[0]+'<br>';
+while(a.length<21){
+    //b = b.concat(' ');
+    a = a.concat('*' , '*');
+    t.innerHTML+=(toStr(a)/*+toStr(b)*/+'<br>');
+};
+// Попытка 2 не удалась, ошибка в коде
+/*var t2=document.getElementById('target2');
+function printEl(c, h=21){
+    var tree=new Array();
+    tree.fill(' ', h-1/c%2, c);
+    tree.fill('*', c);
+    console.log(tree);
+    document.write(toStr(tree)+'<br>');
+}
+var c = 1;
+do{
+    printEl(c);
+    c++
+}while(c<11);
